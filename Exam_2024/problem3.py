343702ad709aad94c47408430c24ee8b71a8c463
MagnusFriisc <hcw@alumni.ku.dk>
2024-06-22 18:18:01 +0200

    2 + 3

--- Exam_2024/problem3.py
+++ Exam_2024/problem3.py
@@ -5,7 +5,6 @@ import param as par
 
 class Interpolation:
     def getA(X,y):
-        #find x points that is bigger than y1 and y2
         xlist= []
         for x in X:
             if x[0]>y[0] and x[1]>y[1]:
@@ -18,7 +17,6 @@ class Interpolation:
                 eDistance.append(np.linalg.norm(x-y))
             return xlist[np.argmin(eDistance)]
     def getB(X, y):
-        # find x points that is bigger than y1 and smaller than y2
         xlist = []
         for x in X:
             if x[0] > y[0] and x[1] < y[1]:
@@ -31,7 +29,6 @@ class Interpolation:
                 eDistance.append(np.linalg.norm(x - y))
             return xlist[np.argmin(eDistance)]
     def getC(X, y):
-        # find x points that is smaller than y1 and y2
         xlist = []
         for x in X:
             if x[0] < y[0] and x[1] < y[1]:
@@ -44,7 +41,6 @@ class Interpolation:
                 eDistance.append(np.linalg.norm(x - y))
             return xlist[np.argmin(eDistance)]
     def getD(X, y):
-        # find x points that is smaller than y1 and bigger than y2
         xlist = []
         for x in X:
             if x[0] < y[0] and x[1] > y[1]:
